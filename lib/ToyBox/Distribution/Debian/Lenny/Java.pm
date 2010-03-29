15562329d16e4fbf8d3ba72c376832c3099c2213
hiroyuki <hiroyuki@dream.niflheimr.org>
2010-03-29 14:52:58 +0900

    update

--- lib/ToyBox/Distribution/Debian/Lenny/Java.pm
+++ lib/ToyBox/Distribution/Debian/Lenny/Java.pm
@@ -5,5 +5,8 @@ use utf8;
 use base qw/ToyBox::Distribution::Debian/;
 
 __PACKAGE__->mk_classdata(pakcages => [qw/sun-java6-jdk sun-java6-demo sun-java6-source sun-java6-doc sun-java6-plugin sun-java6-fonts ant/ ]); 
+__PACKAGE__->mk_classdata(before   => sub {
+    my $url = q{http://java.sun.com/javase/downloads/};
+});
 
 1;
